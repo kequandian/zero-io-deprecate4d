aa5068936e533061959c88c1e46db6291cc3da9d
ye126 <jiajiajia6000@126.com>
2021-06-18 18:57:21 +0800

    取消改名

--- zero-io-fs/src/main/java/com/jfeat/fs/properties/FSProperties.java
+++ zero-io-fs/src/main/java/com/jfeat/fs/properties/FSProperties.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 @ConfigurationProperties(prefix = "io")
-public class FileServerProperties {
+public class FSProperties {
 
     private String fileUploadPath;
 
